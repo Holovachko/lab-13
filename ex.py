cfe51e22ed8ed64d5887d5b99f07e20914b59855
Holovachko <74302993+Holovachko@users.noreply.github.com>
2020-12-27 09:14:32 +0200

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,71 @@
+import math
+
+
+class TVector2D:
+    def __init__(self, x=0, y =0, *args):
+        self.x = x
+        self.y = y
+        self.vector = list(args)
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self,a):
+        self.__x = a
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self,a):
+        self.__y = a
+
+
+
+    def vectors_len(self):
+        return len(self.vector)
+    def rationing_vector(self):
+        return self.x/self.vectors_len(),self.y/self.vectors_len()
+
+    def __gt__(self, other):
+        return self.vectors_len() > other.vectors_len()
+    def __lt__(self, other):
+        return self.vectors_len() < other.vectors_len()
+    def __add__(self, other):
+        return (other.x + self.x , other.y + self.y)
+    def __sub__(self, other):
+        return (self.x - other.x, self.y - other.y)
+    def __mul__(self, other):
+        return (self.x *other.x + self.y * other.y)
+
+
+v = TVector2D(7,1, 1,5,7)
+v2 = TVector2D(10,4,5,9,1)
+print(v + v2)
+print(v.vector)
+print(v.rationing_vector())
+
+
+class TVector3D(TVector2D):
+    def __init__(self, x, y, z, *args):
+        super().__init__(x, y, *args)
+        self.z = z
+    def vector3D_len(self):
+        return math.sqrt(self.x**2+self.y**2+self.z**2)
+
+    def __gt__(self, other):
+        return self.vector3D_len() > other.vector3D_len()
+    def __add__(self, other):
+        return (self.x + other.x , self.y + other.y, self.z + other.z)
+    def __sub__(self, other):
+        return (self.x - other.x , self.y - other.y, self.z - other.z)
+    def __mul__(self, other):
+        return (self.x * other.x + self.y * other.y + self.z * other.z)
+
+q = TVector3D(10,11,6)
+q1 = TVector3D(16,34,5)
+
+if q > q1:
+    print('True')
+else:
+    print('False')
